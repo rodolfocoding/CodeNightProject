0260aa06981f651a9ec0ce93d1cf43852fec00c6
rodolfocoding <rodolfopktd@hotmail.com>
2020-03-11 09:48:38 -0300

    Tela de cadastro

--- CodeNight/src/index.js
+++ CodeNight/src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Login from "./Login";
+import SignUp from "./Cadastro";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Login />, document.getElementById("root"));
+ReactDOM.render(<SignUp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
